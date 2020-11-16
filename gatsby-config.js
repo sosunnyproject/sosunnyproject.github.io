636c97542e85059f609389b6f95771cbadb7701c
sosunnyproject <parksosun1103@gmail.com>
2020-11-16 22:55:29 +0900

    'add-google-analytics'

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,17 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "UA-178004297-2", // Google Analytics / GA
+          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `blog`,
