aed33961ca4539e3bea2c2ba6f51669a1d5675f1
sosunnyproject <parksosun1103@gmail.com>
2020-12-30 08:52:04 +0900

    update index, vitae pages

--- src/pages/vitae.jsx
+++ src/pages/vitae.jsx
@@ -24,7 +24,8 @@ export default function Vitae() {
     </p>
 
     <p>
-      유튜브/줌을 통해 크리에이티브 코딩 101 스터디를 진행할 계획이 있습니다. 관심있으신 분들은 <a target="_blank" rel="noreferrer" href="https://forms.gle/F7MP8cPW8J7aUWfq6">구글폼</a>을 작성해주시면 모집 시 연락드리겠습니다 :)
+      유튜브에 작업 영상과 튜토리얼을 올리고, 트위치를 통해 크리에이티브 코딩 101 스터디를 진행할 계획이 있습니다. 
+      관심있으신 분들은 <a target="_blank" rel="noreferrer" href="https://forms.gle/F7MP8cPW8J7aUWfq6">구글폼</a>을 작성해주시면 모집 시 연락드리겠습니다 :)
       기타 문의사항은 인스타그램 메세지로 연락주세요.
     </p>
 
@@ -33,7 +34,7 @@ export default function Vitae() {
     </p>
 
     <p className={containerStyles.indexBottom}>
-     - 2020/10/12 박소선
+     - 2020/12/30 박소선
     </p>
 
     </div>
