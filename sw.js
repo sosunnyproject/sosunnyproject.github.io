bfd96c378364c5ea3150b2a997dc2c8df559dbdd
sosunnyproject <parksosun1103@gmail.com>
2021-12-19 16:38:38 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-c031ce1c4d5e901838ab.js"
+    "url": "webpack-runtime-044fef035fac937cda80.js"
   },
   {
     "url": "styles.c4551685942c16b0ebab.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "1fc3bb833b3ab6e38a1aa9f5f4d298f7"
+    "revision": "5123db6fe5cccb2d97c7da0b3ba358e0"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-47c8d8b7529f7a984452.js"
@@ -54,7 +54,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "80465630185acdaaa4b229b17cf5be36"
+    "revision": "ae8ab39fb350155d444753ff2c61d22a"
   },
   {
     "url": "polyfill-2355dfec31f56614e456.js"
